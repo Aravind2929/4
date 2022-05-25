53a143006c9f0eeb9fe520a5b8a2dedc02c7ac16
Aravind2929 <aravind.k@access-designs.com>
2022-05-25 17:33:39 +0530

    update

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1> Homepage </h1>
+    <p> lkdn hfouhef ohf iuhviuvc bcjidci uhciudh dhcud hc dc hc chus cuhsa huc hchus hc ushcwiuchw cdcuducud chdc ud cudiuc chuwciuhucudhc duch dch duchudcuhd cd cuhduchduchduc </p>
+    <p> lkdn hfouhef ohf iuhviuvc bcjidci uhciudh dhcud hc dc hc chus cuhsa huc hchus hc ushcwiuchw cdcuducud chdc ud cudiuc chuwciuhucudhc duch dch duchudcuhd cd cuhduchduchduc </p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {}
+}
+</script>
